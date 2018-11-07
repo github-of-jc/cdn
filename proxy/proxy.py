c1dad5c2ace08029b51eee928261f96bbb9c22d9
columbiaawm <columbiaawmwebsite@gmail.com>
2018-11-07 09:31:04 -0500

    changed to correct multithreadding, didn't bind to server fake ip yet

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -11,56 +11,14 @@ import re
 import signal
 import argparse
 
-#./proxy <log> <alpha> <listen-port> <fake-ip> <web-server-ip>
+def connect_client_to_server(conn, addr, threadNum, s, port, LOG, ALPHA, FAKE_IP):
+	print('=====================================beginning of thread=' + str(threadNum))
 
-
-# listen for connections from a browser on any IP address on 
-# the port specified as a command line argument
-
-# accept multiple concurrent connections from web browsers
-# by starting a new thread or process 
-# for each new request
-
-# when proxy connects to a server
-# proxy bind the socket to the fake IP address from command line
-
-parser = argparse.ArgumentParser()
-parser.add_argument('log') 
-parser.add_argument('alpha') 
-# the TCP port the proxy should listen on for accepting connection from browser
-parser.add_argument('listen_port') 
-# proxy bound to fake_ip for outbound connection to web servers
-# fake_ip can only be on of the clients' IP addresses under the network topo specified
-parser.add_argument('fake_ip')
-# ip address of the web server that proxy request video chuncks from 
-# one of the servers IP addresses under the network topolocy specified
-parser.add_argument('server_ip')
-
-args = parser.parse_args()
-LOG, ALPHA, LISTEN_PORT, FAKE_IP, SERVER_IP = args.log, args.alpha, args.listen_port, args.fake_ip, args.server_ip
-print('finished parsing')
-print(LOG, ALPHA, LISTEN_PORT, FAKE_IP, SERVER_IP)
-s = socket.socket()
-port = int(LISTEN_PORT)
-s.bind(('', port))
-print("socket binded to %s" %(port))
-print('created socket, now listen')
-s.listen(5) 
-
-
-while True:
-	print('beginning of client loop')
-	print('# print conn, addr')
-	conn, addr = s.accept()
-	print('Connected by', addr)
-	# open up connection with server
-	# server socket
-	print('created client connection')
 	try:
 		ss = socket.socket()
 		server_ip = FAKE_IP
 		server_port = 8080
-		print('connect proxy to server at server ip', server_ip, ' port ', server_port)
+		print('connect proxy to server at (fake) server ip', server_ip, ' port ', server_port)
 		ss.connect((server_ip, server_port))
 		print('ss connect successful')
 		print('while conn')
@@ -113,7 +71,7 @@ while True:
 			print('try reestablish connection to server')
 			ss = socket.socket()
 			server_ip = FAKE_IP
-			server_port = 8081
+			server_port = 8080
 			print('connect proxy to server at server ip', server_ip, ' port ', server_port)
 			ss.connect((server_ip, server_port))
 			print('ss connect successful')
@@ -121,8 +79,61 @@ while True:
 		except:
 			print('cannot reestablish connection to server, break')
 			conn.close()
-			break
+				
+#./proxy <log> <alpha> <listen-port> <fake-ip> <web-server-ip>
+
+
+# listen for connections from a browser on any IP address on 
+# the port specified as a command line argument
+
+# accept multiple concurrent connections from web browsers
+# by starting a new thread or process 
+# for each new request
+
+# when proxy connects to a server
+# proxy bind the socket to the fake IP address from command line
+
+parser = argparse.ArgumentParser()
+parser.add_argument('log') 
+parser.add_argument('alpha') 
+# the TCP port the proxy should listen on for accepting connection from browser
+parser.add_argument('listen_port') 
+# proxy bound to fake_ip for outbound connection to web servers
+# fake_ip can only be on of the clients' IP addresses under the network topo specified
+parser.add_argument('fake_ip')
+# ip address of the web server that proxy request video chuncks from 
+# one of the servers IP addresses under the network topolocy specified
+parser.add_argument('server_ip')
+
+args = parser.parse_args()
+LOG, ALPHA, LISTEN_PORT, FAKE_IP, SERVER_IP = args.log, args.alpha, args.listen_port, args.fake_ip, args.server_ip
+print('finished parsing')
+print(LOG, ALPHA, LISTEN_PORT, FAKE_IP, SERVER_IP)
+s = socket.socket()
+port = int(LISTEN_PORT)
+s.bind(('', port))
+print("socket binded to %s" %(port))
+print('created socket, now listen')
+s.listen(5) 
+threadNum = 1
+
+while True:
+	print('=====================================beginning of client loop for thread=' + str(threadNum))
+	print('# print conn, addr')
+	conn, addr = s.accept()
+	print('Connected by', addr)
+	# open up connection with server
+	# server socket
+	print('created client connection')
+	thread.start_new_thread(connect_client_to_server, (conn, addr, threadNum, s, port, LOG, ALPHA, FAKE_IP,))
+	threadNum = threadNum + 1
+
 print('end of program')
+
+
+
+
+
 
 
 
