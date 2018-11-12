03dcb2e617c3b5b45486f57f44cede396449ed75
Jiayi Chen <jiayichenjudy@gmail.com>
2018-11-12 02:14:52 -0500

    working with f4m and frag returning

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -18,10 +18,11 @@ def recv_data(threadNum, conn, ss, fake_ip, server_port):
 	print(str(threadNum) + 'cdata:' + str(cdata))
 	print(str(threadNum) + 'recv cdata')
 	packet = None
-	ts = time.time()
 	ismod = False
 	modcdata = None
 	chunk_name = None
+	
+	ts = time.time()
 	print("before conn.recv, ts = " + str(ts))
 	packet = conn.recv(10000)
 	#print(str(threadNum) + 'packet is: \n' + packet)
@@ -29,7 +30,7 @@ def recv_data(threadNum, conn, ss, fake_ip, server_port):
 	get_loc = cdata.find('\n')
 	extension_loc = cdata.find(' ', 4)
 	if cdata[extension_loc-4:extension_loc] == ".f4m":
-		ismod = True
+		ismod = 1
 		modcdata = cdata.replace(".f4m","_nolist.f4m", 1)
 		f = open(str(threadNum)+"cdata_f4m.xml", "w")
 		f.write(cdata)
@@ -37,6 +38,7 @@ def recv_data(threadNum, conn, ss, fake_ip, server_port):
 		print(str(threadNum) + 'received cdata: ============================' + modcdata)
 		return ismod, cdata, modcdata, ts, chunk_name
 	elif cdata[5:8] == "vod":
+		ismod = 2
 		f = open(str(threadNum)+"cdata.xml", "w")
 		print("========!!!!!!\n" + cdata[4:extension_loc])
 		print(cdata[9:extension_loc])
@@ -62,7 +64,7 @@ def reg_send_to_server(chunk_name, logf, alpha, ts, cdata, threadNum, conn, ss, 
 	avgtp = -1
 	chunk_size = -1
 	#throughput
-	tp = -1
+	tp = 10
 	if len(cdata)>0:
 		print('len(cdata>0)')
 		print(ss)
@@ -85,14 +87,14 @@ def reg_send_to_server(chunk_name, logf, alpha, ts, cdata, threadNum, conn, ss, 
 					break
 			tf = time.time()
 			tdiff = tf-ts
+			chunk_size = len(sdata)
 			print("tf: " + str(tf))
 			if len(sdata) > 0:
 				#print(str(threadNum) + "server data is:===============================\n" +  sdata)
 				print(str(threadNum) + 'trying to send client the server data')
-				print("chunk size: " + str(len(sdata)))
+				print("chunk size: " + str(chunk_size))
 				print("calculate throughout:")
-				tp = len(sdata)/(tf-ts)
-	
+				tp = chunk_size/(tf-ts)
 				logf.write(str(tf) + " " + str(tdiff) + " " + str(tp) + " " + str(avgtp) + " " + str(req_bitrate) + " " + str(fake_ip) + " " + str(chunk_name))
 
 				print("throughput: " + str(tp))
@@ -121,7 +123,7 @@ def reg_send_to_server(chunk_name, logf, alpha, ts, cdata, threadNum, conn, ss, 
 	else:
 		print(str(threadNum) + 'client data is empty, break')
 
-def mod_send_to_server(chunk_name, logf, alpha, ts, cdata, modcdata, threadNum, conn, ss, fake_ip, server_port):
+def f4m_send_to_server(chunk_name, logf, alpha, ts, cdata, modcdata, threadNum, conn, ss, fake_ip, server_port):
 	print("#$#$%!$%!#$% IN MODDDDDDD*&^(*(U")
 	print(cdata)
 	print(modcdata)
@@ -132,7 +134,7 @@ def mod_send_to_server(chunk_name, logf, alpha, ts, cdata, modcdata, threadNum, 
 	chunk_size = -1
 	avgtp = -1
 	#throughput
-	tp = -1
+	tp = 10
 	if len(cdata)>0:
 		print('len(cdata>0)')
 		print(ss)
@@ -210,11 +212,83 @@ def mod_send_to_server(chunk_name, logf, alpha, ts, cdata, modcdata, threadNum, 
 	else:
 		print(str(threadNum) + 'client data is empty, break')
 
-def connect_client_to_server(logf, conn, addr, threadNum, s, port, LOG, alpha, FAKE_IP):
+def mod_send_to_server(currtp, chunk_name, logf, alpha, ts, cdata, modcdata, threadNum, conn, ss, fake_ip, server_port):
+	print("#$#$%!$%!#$% IN MODDDDDDD*&^(*(U")
+	print(cdata)
+	print(modcdata)
+	print("received ts: " + str(ts))
+	print("alpha: " + str(alpha))
+	tf = -1
+	req_bitrate = -1
+	chunk_size = -1
+	avgtp = -1
+	#throughput
+	if len(cdata)>0:
+		print('len(cdata>0)')
+		print(ss)
+		sdata = ''
+		
+		try:
+			print(str(threadNum) + "trying to send cdata to ss")
+			ss.send(cdata)
+			print(str(threadNum) + 'cdata sent to server')
+			print(str(threadNum) + 'expecting things back from server')
+			
+			
+			while 1:
+				print(str(threadNum) + 'recv sdata')
+				packet = ss.recv(10000)	
+				if packet:
+					#print(str(threadNum) + 'server packet is: \n' + packet)
+					sdata = sdata + packet
+				else:
+					break
+			tf = time.time()
+			tdiff = tf-ts
+			print("tf: " + str(tf))
+			if len(sdata) > 0:
+				#print(str(threadNum) + "server data is:===============================\n" +  sdata)
+				print(str(threadNum) + 'trying to send client the server data')
+				print("chunk size: " + str(len(sdata)))
+				print("calculate throughout:")
+				newtp = len(sdata)/(tf-ts)
+				currtp = alpha * newtp + (1-alpha) * currtp	
+				logf.write(str(tf) + " " + str(tdiff) + " " + str(currtp) + " " + str(avgtp) + " " + str(req_bitrate) + " " + str(fake_ip) + " " + str(chunk_name))
+				print("throughput: " + str(currtp))
+					
+
+				f = open(str(threadNum)+"modsdata.xml", "w")
+				f.write(sdata)
+				conn.send(sdata)
+				print(str(threadNum) + 'successful sending server data to client')
+				return currtp, tf, chunk_size
+
+		except Exception as e:
+			print("ERROR:" + str(e))
+			print(str(threadNum) + 'uh oh cannot send cdata to server')
+			try:
+				print(str(threadNum) + 'try reestablish connection to server')
+				ss = socket.socket()
+				server_port = 8080
+				print(str(threadNum) + 'connect proxy to server at server ip', fake_ip, ' port ', server_port)
+				ss.connect((fake_ip, server_port))
+				print(str(threadNum) + 'ss connect successful, not sending or receiving tho reg')
+			except Exception as e:
+				print("ERROR:" + str(e))
+				print(str(threadNum) + 'cannot reestablish connection to server, break client connection')
+				conn.close()
+				
+	else:
+		print(str(threadNum) + 'client data is empty, break')
+
+def connect_client_to_server(logf, conn, addr, threadNum, ts, port, LOG, alpha, FAKE_IP):
 	print('=====================================beginning of thread=' + str(threadNum))
 	tp = -1
 	tf = -1
 	chunk_size = -1
+	f4m = 1
+	frag = 2
+	loc_list = []
 	try:
 		ss = socket.socket()
 		fake_ip = FAKE_IP
@@ -235,9 +309,9 @@ def connect_client_to_server(logf, conn, addr, threadNum, s, port, LOG, alpha, F
 			print("exit recv data")
 			print("in main loop ts: " + str(ts))
 			
-			if ismod:
+			if ismod == f4m:
 				print("enter MOD send to server")
-				loc_list, tp, tf, chunk_size = mod_send_to_server(chunk_name, logf, alpha, ts, cdata, modcdata, threadNum, conn, ss, fake_ip, server_port)
+				loc_list, tp, tf, chunk_size = f4m_send_to_server(chunk_name, logf, alpha, ts, cdata, modcdata, threadNum, conn, ss, fake_ip, server_port)
 				print("========= \n tf: " + str(tf) + "\n chunksize: " + str(chunk_size
 	))
 				print("exit MOD send to server in try\n go into reg send")
@@ -246,6 +320,16 @@ def connect_client_to_server(logf, conn, addr, threadNum, s, port, LOG, alpha, F
 				print(str(threadNum) + str(loc_list))
 				print(str(threadNum) + "throughput")
 				print(str(threadNum) + str(tp))
+				print("exit reg send")
+			if ismod == frag:
+				print("enter MOD send to server")
+				tp, tf, chunk_size = mod_send_to_server(tp, chunk_name, logf, alpha, ts, cdata, modcdata, threadNum, conn, ss, fake_ip, server_port)
+				print("========= \n tf: " + str(tf) + "\n chunksize: " + str(chunk_size
+	))
+				print("frag new tp is: " + str(tp))
+				print("exit FRAG send to server in try\n go into reg send")
+				print(str(threadNum) + "loc_list:")
+				print(str(threadNum) + str(loc_list))
 				print("exit reg send")
 			else:
 				print("enter send to server")
@@ -264,7 +348,7 @@ def connect_client_to_server(logf, conn, addr, threadNum, s, port, LOG, alpha, F
 			ss = socket.socket()
 			fake_ip = FAKE_IP
 			server_port = 8080
-			tp, tf, chunk_size = None, None, None
+			tp, tf, chunk_size = 10, None, None
 			print(str(threadNum) + 'connect proxy to server at server ip', fake_ip, ' port ', server_port)
 			ss.connect((fake_ip, server_port))
 			print(str(threadNum) + 'ss connect successful')
@@ -274,13 +358,27 @@ def connect_client_to_server(logf, conn, addr, threadNum, s, port, LOG, alpha, F
 			ismod, cdata, modcdata, ts, chunk_name = recv_data(threadNum, conn, ss, fake_ip, server_port)
 			print("ISMODISMODISMOD")
 			print(ismod)
-			if ismod:
+			if ismod == f4m:
 				print("enter MOD send to server")
-				loc_list, tp, tf, chunk_size = mod_send_to_server(chunk_name, logf, alpha, ts, cdata, modcdata, threadNum, conn, ss, fake_ip, server_port)
+				loc_list, tp, tf, chunk_size = f4m_send_to_server(chunk_name, logf, alpha, ts, cdata, modcdata, threadNum, conn, ss, fake_ip, server_port)
 				print("========= \n tf: " + str(tf) + "\n chunksize: " + str(chunk_size
 	))
 				print("exit MOD send to server in try\n go into reg send")
 				tp = reg_send_to_server(chunk_name, logf, alpha, ts, cdata, threadNum, conn, ss, fake_ip, server_port)
+				print(str(threadNum) + "loc_list:")
+				print(str(threadNum) + str(loc_list))
+				print(str(threadNum) + "throughput")
+				print(str(threadNum) + str(tp))
+				print("exit reg send")
+			if ismod == frag:
+				print("enter MOD send to server")
+				tp, tf, chunk_size = mod_send_to_server(tp, chunk_name, logf, alpha, ts, cdata, modcdata, threadNum, conn, ss, fake_ip, server_port)
+				print("========= \n tf: " + str(tf) + "\n chunksize: " + str(chunk_size
+	))
+				print("frag new tp is: " + str(tp))
+				print("exit FRAG send to server in try\n go into reg send")
+				print(str(threadNum) + "loc_list:")
+				print(str(threadNum) + str(loc_list))
 				print("exit reg send")
 			else:
 				print("enter send to server")
@@ -304,7 +402,7 @@ def connect_client_to_server(logf, conn, addr, threadNum, s, port, LOG, alpha, F
 
 def init_connection(logf, conn, addr, threadNum, s, port, LOG, ALPHA, FAKE_IP):
 	print("init connection at =============================== threadNum: " + str(threadNum))
-	tp = None
+	tp = 10
 	tf = None
 	chunk_size = None
 	tp, tf, chunk_size = connect_client_to_server(logf, conn, addr, threadNum, s, port, LOG, ALPHA, FAKE_IP)
@@ -336,7 +434,7 @@ parser.add_argument('fake_ip')
 parser.add_argument('fake_ip')
 
 args = parser.parse_args()
-LOG, ALPHA, LISTEN_PORT, FAKE_IP, fake_ip = args.log, args.alpha, args.listen_port, args.fake_ip, args.fake_ip
+LOG, ALPHA, LISTEN_PORT, FAKE_IP, fake_ip = args.log, float(args.alpha), args.listen_port, args.fake_ip, args.fake_ip
 print('finished parsing')
 print(LOG, ALPHA, LISTEN_PORT, FAKE_IP, fake_ip)
 s = socket.socket()
